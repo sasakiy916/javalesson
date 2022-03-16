ae2b1cd83b8c067b96d0f18842d16a57f268431e
arukreid <sasaki3y98521j@gmail.com>
2022-03-16 13:55:56 +0900

    Hensu.java追加

---
+++ day2/Hensu.java
@@ -0,0 +1,17 @@
+public class Hensu{
+	public static void main(String[] args){
+		//変数(integer:整数)
+		int money;
+		money = 100;
+		System.out.println("所持金" + money);
+		System.out.println("50円拾った");
+		money = money + 50;
+		System.out.println("所持金" + money);
+		System.out.println("1000円拾った");
+		money = money + 1000;
+		System.out.println("所持金" + money);
+		System.out.println("500円の牛丼食べたよ");
+		money = money - 500;
+		System.out.println("所持金" + money);
+	}
+}
