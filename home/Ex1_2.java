2044b239007558d1b0886dfcf7b545b10434efd9
sasakiy916 <sasaki3y98521j@gmail.com>
2022-03-18 12:49:01 +0900

    homeフォルダ、１章、２章練習問題 追加

---
+++ home/Ex1_2.java
@@ -0,0 +1,9 @@
+public class Ex1_2{
+	public static void main(String[] args){
+		int height = 3;
+		int width = 5;
+		int sum = height * width;
+
+		System.out.println("縦幅 " + height + " 横幅 " + width + " の長方形の面積は、 " + sum);
+	}
+}
---
+++ home/Ex1_3.java
@@ -0,0 +1,15 @@
+public class Ex1_3{
+	public static void main(String[] args){
+		boolean result = true;
+		char character = '駆';
+		double pi = 3.14;
+		long num = 314159265853979L;
+		String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+
+		System.out.println("①" + result);
+		System.out.println("②" + character);
+		System.out.println("③" + pi);
+		System.out.println("④" + num);
+		System.out.println("⑤" + msg);
+	}
+}
