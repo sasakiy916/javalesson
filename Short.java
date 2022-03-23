d4027d3f84bb03a258fd4ab6a5e10f7a0c305a6c
arukreid <sasaki3y98521j@gmail.com>
2022-03-23 10:28:52 +0900

    AgeApp.java 追加

---
+++ Short.java
@@ -0,0 +1,8 @@
+public class Short{
+	public static void main(String[] args){
+	//int a = new java.util.Scanner(System.in).nextInt();
+	double a = (double)3.00;
+	//int a = new java.util.Random().nextInt(11) + -5;
+	System.out.println(a);
+	}
+}
---
+++ home/Slack/sasaki/RpgText.java
@@ -0,0 +1,13 @@
+public class RpgText{
+	public static void main(String[] args){
+		rpgText("aiueo");
+		rpgText("お前はもうすでに死んでいる・・・！");
+		rpgText("ひでぶっ！？");
+	}
+
+	//画面に1行分の文を表示するメソッド
+	public static void rpgText(String text){
+		System.out.println(text);
+		System.out.println("好き");
+	}
+}
