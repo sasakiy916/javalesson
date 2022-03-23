a82ff8190c3427ccc96d9140a9e96b9e0d9b7ebe
sasakiy916 <sasaki3y98521j@gmail.com>
2022-03-23 22:42:54 +0900

    Ques 修正

--- day5/Ques.java
+++ day5/Ques.java
@@ -2,19 +2,22 @@ import java.util.*;
 
 public class Ques{
 	public static void main(String[] args){
-		System.out.println("丁か半か(丁:0,半:1)");
+		System.out.print("丁か半か(丁:0,半:1)>>");
 		int chouhan = new Scanner(System.in).nextInt();
 		int dice1 = new Random().nextInt(6)+1;
 		int dice2 = new Random().nextInt(6)+1;
 
 		int dice = (dice1 + dice2) % 2;
-		if(){
+		System.out.print("出目 " + dice1 + ":" + dice2 + " 結果は ");
+		if(dice == 0){
+			System.out.println("丁");
 		}else{
+			System.out.println("半");
 		}
 		if(dice == chouhan){
-			System.out.println("勝ち");
+			System.out.println("当たり！！");
 		}else{
-			System.out.println("負け");
+			System.out.println("ハズレ！！");
 		}
 	}
 }
