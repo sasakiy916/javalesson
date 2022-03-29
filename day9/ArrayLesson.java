7fa495b1d2d29daad8aaab63147badb8aa46b406
arukreid <sasaki3y98521j@gmail.com>
2022-03-29 11:50:50 +0900

    ArrayLesson 追加

---
+++ day9/ArrayLesson.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class ArrayLesson{
+	public static void main(String[] args){
+		/*
+		 * 配列:値をまとまった塊として扱う技術
+		 */
+		String[] hands={"グー","チョキ","パー"};
+		int pc = new Random().nextInt(hands.length);
+		System.out.print("手を入力(0:グー,1:チョキ,2:パー)>>");
+		int you = new Scanner(System.in).nextInt();
+		System.out.printf("あなたは%s、PCは%s%n",hands[you],hands[pc]);
+		if(pc == you){
+			System.out.println("あいこ");
+		}else if((you+3-pc)%3 == 2){
+			System.out.println("あなたの勝ち");
+		}else{
+			System.out.println("あなたの負け");
+		}
+	}
+}
