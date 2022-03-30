bcbd9a9ac5706102325792fd31276b9895db1dee
arukreid <sasaki3y98521j@gmail.com>
2022-03-30 09:58:20 +0900

    ArrLesson1 追加

---
+++ day10/ArrLesson1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ArrLesson1{
+	public static void main(String[] args){
+		int[] nums = new int[3];
+		System.out.println(nums.length);
+		System.out.println(nums[0]);
+		System.out.println(nums[1]);
+		System.out.println(nums[2]);
+		//System.out.println(nums[3]);//実行時エラー
+
+		//値を入れる
+		nums[0]=10;
+		nums[1]=27;
+		nums[2]=-5;
+	}
+}
