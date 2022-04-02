24e769ca9599abeb7cf0c5aa19b0d154258cd3ae
sasakiy916 <sasaki3y98521j@gmail.com>
2022-04-02 21:20:21 +0900

    14章 終了

---
+++ home/Chapter14/Ex/Account.java
@@ -0,0 +1,23 @@
+public class Account{
+	String accountNumber;
+	int balance;
+
+	public Account(String accountNumber, int balance){
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+	}
+	public String toString(){
+		return "\\" + this.balance + "(口座番号:" + this.accountNumber + ")";
+	}
+
+	public boolean equals(Object o){
+		if(this == o){return true;}
+		if(o instanceof Account){
+			Account a = (Account)o;
+			if(this.accountNumber.trim().equals(a.accountNumber.trim())){
+				return true;
+			}
+		}
+		return false;
+	}
+}
